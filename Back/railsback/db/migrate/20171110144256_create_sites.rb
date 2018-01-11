d1738389b4dbdc149fe48a394834f4212e38d4ac
Vinícius Corrêa de Almeida <viniciusc.almeida1@gmail.com>
2018-01-11 15:52:47 -0200

    Update 20171110144256_create_sites.rb

--- Back/railsback/db/migrate/20171110144256_create_sites.rb
+++ Back/railsback/db/migrate/20171110144256_create_sites.rb
@@ -5,7 +5,7 @@ class CreateSites < ActiveRecord::Migration[5.1]
       t.text :description
       t.string :imgpath
       t.string :category
-      t.picture :string
+      t.string :picture
       
       t.timestamps
     end
