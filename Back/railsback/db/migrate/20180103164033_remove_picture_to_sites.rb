4e217901d4c701b7a82221b764b6f818a83e8a99
ViniciusCdeAlmeida <viniciusc.almeida1@gmail.com>
2018-01-17 18:50:09 -0200

    Cloudinary 3

--- Back/railsback/db/migrate/20180103164033_remove_picture_to_sites.rb
+++
@@ -1,5 +0,0 @@
-class RemovePictureToSites < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :sites, :picture, :string
-  end
-end
--- Back/railsback/db/migrate/20180103164040_add_picture_to_sites.rb
+++
@@ -1,5 +0,0 @@
-class AddPictureToSites < ActiveRecord::Migration[5.1]
-  def change
-    add_column :sites, :picture, :string
-  end
-end
